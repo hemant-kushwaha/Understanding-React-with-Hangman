6c694b18d362ae1d8e45a4fd03d5cbfb779c044f
Hemant Kushwaha <hk8181815252@gmail.com>
2026-03-12 22:40:18 +0530

    Replaced Local URL to  Deployed URL

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,7 @@ function Home () {
     const {setWordList} =useWordStore();//Using zustand-> coming from store
 
     async function fetchWords() {
-        const response = await fetch('http://localhost:3000/words');
+        const response = await fetch('https://understanding-react-with-hangman-backend.onrender.com');
         const data = await response.json();
         console.log(data);
         setWordList([...data]);
